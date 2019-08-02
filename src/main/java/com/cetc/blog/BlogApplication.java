882e19a088ab0fc603198bb99f8c5fe3594adab8
fguby <fguby1995@gmail.com>
2019-08-02 16:08:44 +0800

    添加一个http接口

--- src/main/java/com/cetc/blog/BlogApplication.java
+++ src/main/java/com/cetc/blog/BlogApplication.java
@@ -1,13 +1,38 @@
 package com.cetc.blog;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * EnableDiscoveryClient 开启springcloud的服务注册与发现
+ */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BlogApplication {
+
+    final static Logger log = LoggerFactory.getLogger(BlogApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(BlogApplication.class, args);
     }
 
+
+    @Slf4j
+    @RestController
+    static class TestController {
+
+        @GetMapping("/hello")
+        public String hello(@RequestParam String name) {
+            log.info("invoked name = " + name);
+            return "hello " + name;
+        }
+    }
 }
+
